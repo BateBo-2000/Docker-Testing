6ad8fb74fb9ddcc266d23c2a803a63bb89a0f152
Boyan <boiansinilkov@abv.bg>
2023-08-03 22:13:53 +0300

    client and server working

--- backend_dummy/server.js
+++ backend_dummy/server.js
@@ -1,10 +1,14 @@
 const express = require('express')
+const cors = require('cors');
 const server = express();
 
-server.get("test", ()=>{
-    res.json({"message":"WORKS"}).status(200)
+server.use(cors());
+
+server.get("/test", (req, res)=>{
+    res.json({"message":"SERVER WORKS"}).status(200)
+    console.log('test endpoint hit')
 })
 
-server.listen(3000, ()=>{
-    console.log('server listening on port 3000')
+server.listen(3001, ()=>{
+    console.log('server listening on port 3001')
 })
--- frontend_dummy/src/App.js
+++ frontend_dummy/src/App.js
@@ -1,23 +1,29 @@
-import './App.css';
+import React, { useEffect, useState } from 'react';
 
 function App() {
+  const [server_res, setServerRes] = useState(null);
 
-  let server_res, database_res
-  
+  useEffect(() => {
+    const fetchData = () => {
+      fetch('http://localhost:3001/test')
+        .then((response) => {
+          return response.json();
+        })
+        .then((data) => {
+          setServerRes(data.message);
+        })
+    };
 
-
-
-
-
-
-
+    fetchData();
+  }, []);
 
   return (
     <div className="App">
       <header className="App-header">
         <h1>CLIENT WORKS</h1>
-        <h1></h1>
-        
+        {
+          <h1>{server_res}</h1>
+        }
       </header>
     </div>
   );
